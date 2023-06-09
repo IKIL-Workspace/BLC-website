20208d04d356dd46249fc92cb3d30b48ff6afeaa
Da Blockchain Dev <79978961+datechlord@users.noreply.github.com>
2023-06-09 18:36:53 +0100

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -29,7 +29,16 @@ const HeroSection = ({ isMenuShown }) => {
         muted
         className="object-cover h-full w-full absolute -z-10"
       />
-
+ <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              Announcing our next round of funding.{' '}
+              <a href="#" className="font-semibold text-indigo-600">
+                <span className="absolute inset-0" aria-hidden="true" />
+                Read more <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
       <div
         className={`p-8 flex flex-col items-center justify-center duration-500 w-full ${
           isVideoPlaying ? "" : "bg-black/80"
