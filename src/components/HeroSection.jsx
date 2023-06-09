276b3c6ac9f9221b26db50472b78f4d8d97f0473
Da Blockchain Dev <79978961+datechlord@users.noreply.github.com>
2023-06-09 20:58:03 +0100

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -31,42 +31,7 @@ const HeroSection = ({ isMenuShown }) => {
         className="object-cover h-full w-full absolute -z-10"
       />
 
-      <div
-        className={`banner-pad flex flex-col items-center justify-center duration-500 w-full ${
-          isVideoPlaying ? "opacity-50" : "bg-black/80"
-        } ${isMenuShown ? "opacity-20" : "opacity-100"}`}>
-
-
-          <div className="main-h1">
-						<img src="#" className="logo-img"/>
-						<h1>Billion Local Coin </h1>
-					</div>
-
-        {/* <h1 className="text-5xl lg:text-7xl capitalize mb-12">
-          the the <span className="text-thBlue font-bold">future</span> is here
-        </h1> */}
-
-        <div className="flex flex-row justify-center items-center">
-          <GradientBtn className="capitalize" title="get anton" />
-          <GradientBtn className="capitalize mx-12" title="products" />
-
-          {isVideoPlaying ? (
-            <FaPauseCircle
-              size={30}
-              onClick={handleVideoPause}
-              className="cursor-pointer hover:scale-110 duration-200 text-thBlue"
-            />
-          ) : (
-            <FaPlayCircle
-              size={30}
-              onClick={handleVideoPlay}
-              className="cursor-pointer hover:scale-110 duration-200 text-thBlue"
-            />
-          )}
-        </div>
-
-
-      </div>
+      
     </div>
   );
 };
