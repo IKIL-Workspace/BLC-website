28f2a40e00ee7d51cfc04a7ffb7d26951097e0c8
Da Blockchain Dev <79978961+datechlord@users.noreply.github.com>
2023-06-09 21:27:58 +0100

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -33,7 +33,7 @@ const HeroSection = ({ isMenuShown }) => {
   
     <div
       className={`p-8 flex flex-col items-center justify-center duration-500 w-full ${
-        isVideoPlaying ? "" : "bg-black/80"
+        isVideoPlaying ? "bg-black/80" : "bg-black/80"
       } ${isMenuShown ? "opacity-20" : "opacity-100"}`}
     >
       <div className="main-h1">
