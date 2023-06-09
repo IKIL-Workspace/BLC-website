a772860f7c84158981bc27c52cfe3797da0bb0c9
Da Blockchain Dev <79978961+datechlord@users.noreply.github.com>
2023-06-09 23:46:19 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = ({ isMenuShown, setIsMenuShown }) => {
         <div className="flex justify-between items-center max-w-screen-xl mx-auto px-4 h-full">
 
           <div>
-            <h1 className="text-thBlue tracking-widest text-3xl"><img src={logo} alt="" className="d-inline-block align-text-top brand-img"/> BLC Coin</h1>
+            <h1 className="tracking-widest text-2xl"><img src={logo} alt="" className="d-inline-block align-text-top brand-img"/> BLC Coin</h1>
           </div>
 
           <div className="hidden lg:flex items-center">
