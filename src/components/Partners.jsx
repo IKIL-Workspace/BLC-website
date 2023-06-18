1b5563d48e3d438cff4e006e459c8f51d394da51
Da Blockchain Dev <79978961+datechlord@users.noreply.github.com>
2023-06-18 09:51:44 +0100

    Added video

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -6,7 +6,7 @@ import coinmarketcap from "../assets/Coinmarketcap-m-logo-3.png";
 import pinksale from "../assets/Pinksale-m-logo-4.png";
 import Subtitle from "./Subtitle";
 import style from "../assets/style.css"
-import heroVideo from "../assets/hero.mp4";
+import PartnerVideo from "../assets/partner.mp4";
 
 
 const Partners = ({isMenuShown}) => {
@@ -28,7 +28,7 @@ const Partners = ({isMenuShown}) => {
     <div className="">
     <video
       ref={videoRef}
-      src={heroVideo}
+      src={PartnerVideo}
       autoPlay
       loop
       muted
