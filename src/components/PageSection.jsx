bdff7f88958c2ca4c93a0c378feab9fb08df6370
Da Blockchain Dev <79978961+datechlord@users.noreply.github.com>
2023-06-18 08:26:22 +0100

    update

--- src/components/PageSection.jsx
+++ src/components/PageSection.jsx
@@ -6,7 +6,6 @@ const PageSection = ({ name, title, subtitle, children }) => {
       <div className="my-32 mx-8 text-center lg:text-left">
         <h1 className="text-5xl">{title}</h1>
         <p className="mt-8 mb-16">{subtitle}</p>
-
         {children}
       </div>
     </div>
--- src/components/Subtitle.jsx
+++ src/components/Subtitle.jsx
@@ -4,7 +4,7 @@ const PageSection = ({ name, title, subtitle, children }) => {
   return (
     <div name={name} className="max-w-screen-xl mx-auto h-fit">
       <div className="my-32 mx-8 text-center lg:text-left">
-        <h1 className="text-5xl">{title}</h1>
+        <h1 className="partnerhead">{title}</h1>
         <p className="mt-8 mb-16">{subtitle}</p>
 
         {children}
