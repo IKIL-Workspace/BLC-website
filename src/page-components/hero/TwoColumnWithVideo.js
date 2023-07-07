c988e9f18dc93ee7aa86682cd577148584cb2db7
KestonFraser <keston.fraser.development@gmail.com>
2023-07-07 00:10:13 -0400

    Updated About page

--- src/page-components/hero/TwoColumnWithVideo.js
+++ src/page-components/hero/TwoColumnWithVideo.js
@@ -26,7 +26,7 @@ const Paragraph = tw.p`my-5 lg:my-8 text-sm lg:text-base font-medium text-gray-6
 const Actions = tw.div`flex flex-col items-center sm:flex-row justify-center lg:justify-start mt-8`;
 const PrimaryButton = tw.button`font-bold px-8 lg:px-10 py-3 rounded bg-primary-500 text-black hocus:bg-temp-200 focus:shadow-outline focus:outline-none transition duration-300`;
 const WatchVideoButton = styled.button`
-  ${tw`mt-4 sm:mt-0 sm:mr-8 flex items-center text-secondary-300 transition duration-300 hocus:text-temp-200 focus:outline-none`}
+  ${tw`mb-4 sm:mb-0 sm:mr-8 flex items-center text-secondary-300 transition duration-300 hocus:text-temp-200 focus:outline-none`}
   .playIcon {
     ${tw`stroke-1 w-12 h-12`}
   }
